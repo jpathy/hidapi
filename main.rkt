14b81957a39aee4ea41528a870ed9ce272390a4f
Jiten Pathy <jpathy@lazycons.xyz>
2017-10-16 14:38:59 +0530

    minor cleanup: formatting, dead code removal.

--- main.rkt
+++ main.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require (rename-in "private/ffi.rkt" (hid-enumerate hid-enumerate-ffi))
+(require (rename-in "private/ffi.rkt"
+                    [hid-enumerate hid-enumerate-ffi])
          racket/contract
          racket/list
          (except-in "private/ffi.rkt"
@@ -22,24 +23,21 @@
 (define (hid-enumerate #:vendor-id [v 0] #:product-id [p 0])
   (define info-list (hid-enumerate-ffi v p))
   (begin0
-      (for/list ([info info-list])
-        (apply hid-device-info
-               ;; necessary to copy C alloc'd strings to racket values
-               ;; since we free the values using `hid-free-enumeration`
-               (drop-right (map (lambda(e)
-                                  (cond
-                                    [(string? e) (string-copy e)]
-                                    [(bytes? e) (bytes->path e)]
-                                    [else e]))
-                                (hid_device_info->list info))
-                           1)))
+    (for/list ([info info-list])
+      (apply hid-device-info
+             ;; necessary to copy C alloc'd strings to racket values
+             ;; since we free the values using `hid-free-enumeration`
+             (drop-right (map (lambda(e)
+                                (cond
+                                  [(string? e) (string-copy e)]
+                                  [(bytes? e) (bytes->path e)]
+                                  [else e]))
+                              (hid_device_info->list info))
+                         1)))
     (hid-free-enumeration info-list)))
 
 (define (hid-write-full device bstr)
   (hid-write device bstr (bytes-length bstr)))
-
-(define (hid-send-feature-report-full device bstr)
-  (hid-send-feature-report device bstr (bytes-length bstr)))
 
 (define (hid-read-bytes device size)
   (let* ([bstr (make-bytes size)]
@@ -61,8 +59,8 @@
  (contract-out
   [hid-init (-> void?)]
   [hid-exit (-> void?)]
-  [hid-enumerate (->* []
-                      [#:vendor-id integer? #:product-id integer?]
+  [hid-enumerate (->* ()
+                      (#:vendor-id integer? #:product-id integer?)
                       (listof hid-device-info?))]
   [hid-open (-> #:vendor-id integer? #:product-id integer? #:serial-number string?
                 (or/c hid-device? #f))]
@@ -74,7 +72,7 @@
                    [n (s)
                       (and/c integer?
                              (<=/c (bytes-length s)))])
-                  [r integer?])]
+                  (r integer?))]
   [hid-write-full (-> hid-device? bytes? integer?)]
   [hid-read-timeout (->i ([d hid-device?]
                           [s bytes?]
@@ -82,13 +80,13 @@
                              (and/c integer?
                                     (<=/c (bytes-length s)))]
                           [t integer?])
-                         [r integer?])]
+                         (r integer?))]
   [hid-read (->i ([d hid-device?]
                   [s bytes?]
                   [n (s)
                      (and/c integer?
                             (<=/c (bytes-length s)))])
-                 [r integer?])]
+                 (r integer?))]
   [hid-read-bytes/timeout (-> hid-device? integer? integer? bytes?)]
   [hid-read-bytes (-> hid-device? integer? bytes?)]
   [hid-get-feature-report (->i ([_ hid-device?]
@@ -96,13 +94,13 @@
                                 [n (s)
                                    (and/c integer?
                                           (<=/c (bytes-length s)))])
-                               [r integer?])]
+                               (r integer?))]
   [hid-send-feature-report (->i ([d hid-device?]
                                  [s bytes?]
                                  [n (s)
                                     (and/c integer?
                                            (<=/c (bytes-length s)))])
-                                [r integer?])]
+                                (r integer?))]
   [hid-get-manufacturer-string (-> hid-device? integer? string?)]
   [hid-get-product-string (-> hid-device? integer? string?)]
   [hid-get-serial-number-string (-> hid-device? integer? string?)]
